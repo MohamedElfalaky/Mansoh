cfcf9e5369270b308acf81ea18353aeb5b0fede6
Ahmed Ali <148041691+AhmedAli1994@users.noreply.github.com>
2023-12-06 14:41:26 +0200

    icons in chat

--- lib/Data/repositories/authentication/mob_repo.dart
+++ lib/Data/repositories/authentication/mob_repo.dart
@@ -17,8 +17,12 @@ class MobRepo {
           body: {
             'mobile': '$phone',
           });
+      print("response.body");
+      print(response.statusCode);
       Map<String, dynamic> responseMap = json.decode(response.body);
-      if (response.statusCode == 200 && responseMap["status"] == 1) {
+      if (response.statusCode == 201 && responseMap["status"] == 1) {
+        print("response.body");
+
         final userdata = mobModelFromJson(responseMap);
         return userdata;
       } else {
