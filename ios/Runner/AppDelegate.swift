26659d900b48b54ecc9496245aeeb16ecea83ff9
Ahmed Ali <Ahmed-minyo@users.noreply.github.com>
2026-09-03 12:00:07 +0300

    up

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -19,7 +19,7 @@ import Flutter
 import Firebase
 import FirebaseMessaging
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
